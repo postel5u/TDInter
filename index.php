44031cc43f6915b4410eae5dc6a0176edf288417
giovanne3u <giovannelli.alexis@gmail.com>
2017-01-25 16:59:23 +0100

    recupere lat et lon du client

--- index.php
+++ index.php
@@ -3,17 +3,12 @@
   $opts = array('http' => array('proxy' => 'www-cache:3128', "request_fulluri" => true));
   $context = stream_context_create($opts);
 
-
   $clientIp = $_SERVER['REMOTE_ADDR'];
-
-  var_dump($clientIp);
 
   $local = simplexml_load_string(file_get_contents("http://ip-api.com/xml/37.168.5.201", false, $context));
 
-  var_dump($local->query[0]);
-
-  $long = $local->query->lon;
-  $lat = $local->query->lat;
+  $long = $local->lon;
+  $lat = $local->lat;
 
   echo ("Longitude : ".$long."<br/>"."Lattitude : ".$lat);
 
