106cb62709355631d44d733500211e0b539ef5e2
giovanne3u <giovannelli.alexis@gmail.com>
2017-01-25 16:48:01 +0100

    Config proxy

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 
+  $opts = array('http' => array('proxy' => 'www-cache:3128', "request_fulluri" => true));
+  $context = stream_context_create($opts);
+
+
   $clientIp = $_SERVER['REMOTE_ADDR'];
 
   var_dump($clientIp);
 
-  $local = simplexml_load_string(file_get_contents("http://ip-api.com/xml/37.168.5.201"));
+  $local = simplexml_load_string(file_get_contents("http://ip-api.com/xml/37.168.5.201", $context));
 
   var_dump($local->query[0]);
 
