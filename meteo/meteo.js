611360d75f5db1cec959ad4ee68abf83540a3076
debian <alexandre.pierre5@etu.univ-lorraine.fr>
2017-02-01 15:33:26 +0100

    resolution de bugs

--- meteo/meteo.js
+++ meteo/meteo.js
@@ -3,23 +3,15 @@ $.ajax( {
   type: 'GET',
   dataType: 'jsonp',
   success: function(location) {
-     $.ajax({
-       url: 'http://ip-api.com/xml/'+location.ip,
-       type: 'GET',
-       success: function(xml){
 
-         var mymap = L.map('mapid').setView([$(xml).find('lat')[0].textContent,$(xml).find('lon')[0].textContent], 13);
-         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-             maxZoom: 18,
-             id: 'your.mapbox.project.id',
-             accessToken: 'your.mapbox.public.access.token'
-         }).addTo(mymap);
-       },
-       error: function(e){
-         console.log("test");
-       }
-     });
+    var mymap = L.map('mapid').setView([location.latitude ,location.longitude ], 13);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'your.mapbox.project.id',
+        accessToken: 'your.mapbox.public.access.token'
+    }).addTo(mymap);
   },
   error: function(e){
     console.log(e);
